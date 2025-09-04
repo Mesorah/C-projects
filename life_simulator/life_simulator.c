d8a2bf5653d097b02ea582a8bfe816572b18d278
Messorah <garabrielra@gmail.com>
2025-09-04 18:25:58 -0300

    typedef used in place of pointers

--- life_simulator/life_simulator.c
+++ life_simulator/life_simulator.c
@@ -3,47 +3,67 @@
 #include <stdlib.h>
 
 
-struct ProfileStats {
+typedef struct {
     char username[20];
     int energy;
     float money;
-};
+} ProfileStats;
 
 
-void work(int *energy, float *money) {
-    float money_earned = 10;
-    
-    *energy -= 50;
-    *money += money_earned;
+typedef struct {
+    ProfileStats profile_stats;
+    float money_earned;
+} WorkResult;
+
+
+WorkResult work_data() {
+    WorkResult work_result = {0};
+
+    work_result.money_earned = 10; // Hard coded
+
+    work_result.profile_stats.money += work_result.money_earned;
+
+    work_result.profile_stats.energy -= 50;
+
+    return work_result;
+}
+
+
+void work() {
+    WorkResult work_result = work_data();
 
     printf(
         "voce trabalhou duro! \n"
         "e conseguiu: %0.2f reais e agora voce tem no total: %0.2f reais, \n"
-        "mas sua energia agora é: %d", 
-        money_earned, *money, *energy
+        "mas sua energia agora eh: %d \n", 
+        work_result.money_earned,
+        work_result.profile_stats.money,
+        work_result.profile_stats.energy
     );
 }
 
 
-void menu(int *energy, float *money) {
+void menu() {
     int choice;
 
-    printf(
-        "Digite \n"
-        "[1] para trabalhar \n"
-        "[2] para descansar \n"
-        "[3] para ir a loja \n"
-        "[4] para sair do jogo \n"
-    );
+    while (1) {
+        printf(
+            "Digite \n"
+            "[1] para trabalhar \n"
+            "[2] para descansar \n"
+            "[3] para ir a loja \n"
+            "[4] para sair do jogo \n"
+        );
 
-    scanf("%d", &choice);
+        scanf("%d", &choice);
 
-    if (choice == 1) work(energy, money);
+        if (choice == 1) work();
+    }
 }
 
 
 void profile() {
-    struct ProfileStats profile_stats;
+    ProfileStats profile_stats;
 
     printf("Digite seu nome: ");
     scanf("%s", profile_stats.username);
@@ -61,7 +81,7 @@ void profile() {
         profile_stats.username, profile_stats.money, profile_stats.energy
     );
 
-    menu(&profile_stats.energy, &profile_stats.money);
+    menu();
 }
 
 
